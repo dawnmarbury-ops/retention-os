9443fe664d183a8e68b837e0da2279d8afb71ae8
dawnmarbury-ops <dawnmarbury@yahoo.com>
2026-05-14 18:48:23 -0400

    Create manual_assets.py

---
+++ scripts/connectors/manual_assets.py
@@ -0,0 +1,26 @@
+def get_positions():
+    """
+    Current Ledger and Staked holdings while we finalize API connectors.
+    """
+    return [
+        {
+            "symbol": "WELL",
+            "qty": 0.0,              # <-- Put your WELL amount here
+            "location": "Crypto.com Mobile App"
+        },
+        {
+            "symbol": "LYX",
+            "qty": 1200.0, 
+            "location": "Lukso Vault (staked)"
+        },
+        {
+            "symbol": "RSR",
+            "qty": 0.0,              # <-- Put your RSR amount here
+            "location": "Ledger (Cold Storage)"
+        },
+        {
+            "symbol": "HBAR",
+            "qty": 0.0,              # <-- Put your HBAR amount here
+            "location": "Ledger (Cold Storage)"
+        }
+    ]
